7db0dfa84f4a5cc934631ded061a4d5d4f70ad95
Willsem <happymainer132@yandex.ru>
2019-11-07 00:42:59 +0300

    [EDT] drawing only visible objects

--- src/source/Managers/DrawManager.cc
+++ src/source/Managers/DrawManager.cc
@@ -5,6 +5,7 @@ void DrawManager::draw(ObjectIterator begin, ObjectIterator end, Drawer &drawer)
     ObjectIterator iter = begin;
     for (; iter != end; ++iter) {
         std::shared_ptr< Object > obj = *iter;
-        obj->draw(drawer);
+        if (obj->isVisible())
+            obj->draw(drawer);
     }
 }
